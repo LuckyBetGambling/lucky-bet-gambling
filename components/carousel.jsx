c981c9fccf3fb5886c518a3462c6d22792d654d9
Erik Follette <erikfollette@fastmail.fm>
2023-01-30 11:14:07 -0800

    feat: create basic buttons for slideshow

--- components/carousel.jsx
+++ components/carousel.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import Carousel from 'react-gallery-carousel';
 import 'react-gallery-carousel/dist/index.css';
+import styled from 'styled-components';
+
+// TODO: Create better looking left/rghtIcons
+const Left = styled.div`
+    height: 50px;
+    width: 50px;
+    border-radius: 50px;
+    background-color: red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Right = styled.div`
+    height: 50px;
+    width: 50px;
+    border-radius: 50px;
+    background-color: red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const Carouse1 = ({carouselItems, title, styles}) => {
 
@@ -20,6 +42,8 @@ const Carouse1 = ({carouselItems, title, styles}) => {
           shouldSwipeOnMouse={false} // for selecting text
           shouldMinimizeOnSwipeDown={false} 
           style={{ userSelect: 'text', ...styles }}
+          leftIcon={<Left>back</Left>}
+          rightIcon={<Right>next</Right>}
         >
           {carouselItems.map(
             (item) => {
